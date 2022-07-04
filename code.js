b471f9f2e16744f46f2b4baf14bb6c65cb8705a8
Igor <daedsironoh@gmail.com>
2022-07-04 18:21:51 +0500

    basic options ok

---
+++ code.js
@@ -0,0 +1,16 @@
+Vue.createApp({
+    data: () => ({
+        newElem: '',
+        listNewElem: [],
+        listElem: [],
+    }),
+    methods: {
+        addNewElems(){
+            if (this.newElem !== '') {
+                this.listNewElem = this.newElem.split(';')
+                this.listElem.push(...this.listNewElem)
+                this.newElem = ''
+            }
+        }
+    }
+}).mount('#app')
